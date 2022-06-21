f68940b0221906575729aa53fdb4643f02c39917
Alex <skyriver.f1@gmail.com>
2022-06-21 17:03:40 +0500

    added story block & more

---
+++ marmelad/_blocks/story-block/story-block.js
@@ -0,0 +1,14 @@
+function initStoryBlockSlider() {
+  const swiper = new Swiper(".js-story-block-slider", {
+    slidesPerView: 1,
+    spaceBetween: 100,
+    autoHeight: true,
+    effect: "fade",
+    navigation: {
+      nextEl: ".story-block__buttons .swiper-button-next",
+      prevEl: ".story-block__buttons .swiper-button-prev",
+    },
+  });
+}
+
+initStoryBlockSlider();

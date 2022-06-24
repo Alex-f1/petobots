2bb53df14bf02b835f670444f7a4e89ffd1002ae
Alex <skyriver.f1@gmail.com>
2022-06-24 20:39:04 +0500

    all

--- marmelad/_blocks/main-menu/main-menu.js
+++ marmelad/_blocks/main-menu/main-menu.js
@@ -1,3 +1,30 @@
 $('.js-button-burger').on('click', function () {
   $('.main-menu').toggleClass('_is-show');
 });
+
+function navToAnchor() {
+  $('.js-nav-to-anchor a').on('click', function (e) {
+    e.preventDefault();
+    var anchor = $(this);
+    $('.main-menu').removeClass('_is-show');
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000);
+    return false;
+  });
+}
+
+navToAnchor();
+
+function fixedPanel() {
+  var fixedPanel = $('.fixed-panel');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 150 && fixedPanel.hasClass('fixed-panel')) {
+      fixedPanel.addClass('fixed');
+    } else if ($(this).scrollTop() <= 150 && fixedPanel.hasClass('fixed')) {
+      fixedPanel.removeClass('fixed');
+    }
+  });
+}
+
+fixedPanel();
